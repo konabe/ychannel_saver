6d3a917da77d72653d8cb5db8a87301079f56773
rkonabe <rt.konabe@gmail.com>
2017-09-30 19:56:16 +0900

    一ヶ月ずつ500件取得させることにより、500件以上の動画のあるチャンネルに対応。

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from auth import get_authenticated_service
 import os
 import re
 import urllib
+from copy import deepcopy
 
 from oauth2client.tools import argparser
 
 from pytube import YouTube
 
 import dateutil.parser
-from datetime import datetime
+from datetime import datetime, timedelta
 
 data_dir_name = os.path.join(os.path.dirname(__file__), 'data')
+
+NO_DOWNLOAD = True
 
 def main():
 
@@ -40,71 +43,98 @@ def main():
     #retrieve videos of channel which you can save.
     init_flag = True; kwargs = {}; count = 0
     finish_flag = False
-    #TODO cannot get more than 500 videos at once.
-    while True:
-        if init_flag:
-            kwargs['part'] = 'id, snippet'
-            kwargs['channelId'] = channel.id
-            kwargs['maxResults'] = 50
-            kwargs['order'] = 'date'
-            kwargs['type'] = 'video'
-            #kwargs['publishedBefore'] = "2015-06-17T07:07:37Z"
-            init_flag = False
-        else:
-            kwargs['pageToken'] = nextPageToken
+    #TODO チャンネル立ち上げ日の取得から取得開始日時を取得
+    start_published = datetime(2005,2,1) # the date youtube started
+    tmp_published = None
+    end_publised = datetime.now()
 
-        results = service.search().list(**kwargs).execute()
-        try:
-            nextPageToken = results['nextPageToken']
-        except KeyError:
-            finish_flag = True
+    while True: # get 500 videos per month
 
-        items = results['items']
+        tmp_published = deepcopy(start_published)
+        next_month = tmp_published.month + 1 if tmp_published.month!= 12 else 1
+        next_year = tmp_published.year + 1 if tmp_published.month == 12 else tmp_published.year
+        tmp_published = tmp_published.replace(year=next_year, month=next_month)
 
-        print('\n[DOWN LOAD]\n')
+        print("date : " + start_published.isoformat() +
+                " =====> " + tmp_published.isoformat())
 
-        for item in items:
-            #anaylze the videos
-            count += 1
-            video_title = item['snippet']['title']
-            #file_name check
-            video_title = re.sub(r'[\\|/|"|<|>|\|]', '***', video_title)
-            video_title = re.sub(r'[?]', '？', video_title)
-            video_title = re.sub(r'[:]', '：', video_title)
-            video_id = item['id']['videoId']
-            video_kwargs = {
-                'part' : 'snippet',
-                'id' : video_id
-            }
-            video_results = service.videos().list(**video_kwargs).execute()
-            date = video_results['items'][0]['snippet']['publishedAt']
-            parsed_date = dateutil.parser.parse(date)
-            date = parsed_date.strftime('%Y%m%d%H%M%S')
-            file_name = date + ' ' + video_title
-
-            #prepare for download videos
-            url = "https://www.youtube.com/watch?v=" + video_id
-            yt = YouTube(url)
-            yt.set_filename(file_name)
-            data = yt.filter('mp4')[-1]
-
-            if not os.path.exists(channel_dir_name):
-                os.mkdir(channel_dir_name)
-
-            print('[No.%d] %s' % (count, video_title))
-            print('Date : %s' % parsed_date)
-            try:
-                data.download(channel_dir_name)
-            except (FileExistsError, OSError):
-                print('the file already exists. skipped downloading. \n')
-                continue
-
-            print('Download completed : %s' % file_name+'.mp4')
-            print()
-
-        print()
-        if finish_flag:
+        if tmp_published > end_publised:
+            print("date finished")
             break
+
+        init_flag = True
+        finish_flag = False
+
+        while True:
+
+            if init_flag:
+                kwargs['part'] = 'id, snippet'
+                kwargs['channelId'] = channel.id
+                kwargs['maxResults'] = 50
+                kwargs['order'] = 'date'
+                kwargs['type'] = 'video'
+                kwargs['publishedAfter'] = start_published.isoformat("T") + "Z"
+                kwargs['publishedBefore'] = tmp_published.isoformat("T") + "Z"
+                init_flag = False
+            else:
+                kwargs['pageToken'] = nextPageToken
+
+            results = service.search().list(**kwargs).execute()
+            try:
+                nextPageToken = results['nextPageToken']
+            except KeyError:
+                finish_flag = True
+
+            items = results['items']
+
+            for item in items[::-1]:
+                #anaylze the videos
+                count += 1
+                video_title = item['snippet']['title']
+                #file_name check
+                video_title = re.sub(r'[\\|/|"|<|>|\|]', '***', video_title)
+                video_title = re.sub(r'[?]', '？', video_title)
+                video_title = re.sub(r'[:]', '：', video_title)
+                video_id = item['id']['videoId']
+                video_kwargs = {
+                    'part' : 'snippet',
+                    'id' : video_id
+                }
+                video_results = service.videos().list(**video_kwargs).execute()
+                date = video_results['items'][0]['snippet']['publishedAt']
+                parsed_date = dateutil.parser.parse(date)
+                date = parsed_date.strftime('%Y%m%d%H%M%S')
+                file_name = date + ' ' + video_title
+
+                #prepare for download videos
+                url = "https://www.youtube.com/watch?v=" + video_id
+                yt = YouTube(url)
+                yt.set_filename(file_name)
+                data = yt.filter('mp4')[-1]
+
+
+                if not os.path.exists(channel_dir_name):
+                    os.mkdir(channel_dir_name)
+
+                print('[No.%d] %s' % (count, video_title))
+                print('Date : %s' % parsed_date)
+                if(not NO_DOWNLOAD):
+                    try:
+                        data.download(channel_dir_name)
+                    except (FileExistsError, OSError):
+                        print('the file already exists. skipped downloading. \n')
+                        continue
+                    print('Download completed : %s' % file_name+'.mp4')
+                print()
+
+            print()
+            if finish_flag:
+                break
+        try:
+            kwargs.pop('pageToken')
+        except(KeyError):
+            pass
+        start_published = deepcopy(tmp_published)
 
     print("Finish!")
 
